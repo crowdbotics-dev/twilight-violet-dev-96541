d9f2f61f0dfd2047c281e7722c882fe198d70bfb
Crowdbotics <engineering@crowdbotics.com>
2023-07-01 23:16:10 +0000

    Initial commit.

---
+++ backend/twilight_violet_dev_96541/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for twilight_violet_dev_96541 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twilight_violet_dev_96541.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-violet-dev-96541",
+  url: "https://twilight-violet-dev-96541.botics.co",
+  api: "https://twilight-violet-dev-96541.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
